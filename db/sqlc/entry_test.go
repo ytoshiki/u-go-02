d79bd7dbab723166e145d304aabb1b0495e55542
ysoktsk <hnkc.ysoktsk@gmail.com>
2023-11-26 00:01:39 +0900

    Write unit tests for database CRUD with random data in Golang

--- db/sqlc/entry_test.go
+++ db/sqlc/entry_test.go
@@ -1,7 +1,72 @@
 package db
 
-import "testing"
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/ytoshiki/simplebank/util"
+)
+
+func createRandomEntry(t *testing.T, account Account) Entry {
+
+	arg := CreateEntryParams{
+		AccountID: account.ID,
+		Ammount: util.RandomMoney(),
+	}
+	entry, err := testQueries.CreateEntry(context.Background(), arg)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, entry)
+
+	require.Equal(t, entry.Ammount, arg.Ammount)
+	require.Equal(t, entry.AccountID, arg.AccountID)
+
+	require.NotZero(t, entry.ID)
+	require.NotZero(t, entry.CreatedAt)
+	
+	return entry
+}
 
 func TestCreateEntry(t *testing.T) {
+	account := createRandomAccount(t)
+	createRandomEntry(t, account)
+}
 
+func TestGetEntry(t *testing.T) {
+	account := createRandomAccount(t)
+	entry1 := createRandomEntry(t, account)
+
+	entry2, err := testQueries.GetEntry(context.Background(), entry1.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, entry2)
+
+	require.Equal(t, entry1.AccountID, entry2.AccountID)
+	require.Equal(t, entry1.Ammount, entry2.Ammount)
+	require.Equal(t, entry1.ID, entry2.ID)
+	require.WithinDuration(t, entry1.CreatedAt, entry2.CreatedAt, 1)
+}
+
+func TestListEntries(t *testing.T) {
+	account := createRandomAccount(t)
+
+	for i := 0; i < 10; i++ {
+		createRandomEntry(t, account)
+	}
+
+	arg := ListEntriesParams{
+		AccountID: account.ID,
+		Limit:     5,
+		Offset:    5,
+	}
+
+	entries, err := testQueries.ListEntries(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, entries, 5)
+
+	for _, entry := range entries {
+		require.NotEmpty(t, entry)
+		require.Equal(t, arg.AccountID, entry.AccountID)
+	}
 }
